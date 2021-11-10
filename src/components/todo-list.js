0a388987bd00f9928e9d67634fa88f7f663e2a8f
kadyrbek-kurenkeev <87137828+kadyrbek-kurenkeev@users.noreply.github.com>
2021-11-10 00:09:28 -0800

    main

---
+++ src/components/todo-list.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const TodoList = () => {
+    return (
+      <div>
+        <ul>
+          <li> <span>Task 1</span></li>
+          <li> <span>Task 2</span></li>
+          <li> <span>Task 3</span></li>
+        </ul>
+      </div>
+    )
+  }
+
+  export default TodoList;
